867447dd2e9954b50161e3b7d32651128c37982e
Shiv Raj Pant <shivrajpant76@gamil.com>
2024-09-06 17:03:32 +0545

    add loops with array

---
+++ practice qs/17th day/loops_with_array.js
@@ -0,0 +1,9 @@
+let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+for (let i = 0; i <fruits.length; i++){
+    console.log(fruits[i]);
+}
+console.log("backward");
+
+for (let i = 0; i >= fruits.length - 1; i--){
+    console.log(fruits[i]);
+}
