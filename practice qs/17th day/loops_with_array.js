ec2b9983efc3d023420fddd93740576c8cc62680
Shiv Raj Pant <shivrajpant76@gamil.com>
2024-10-17 15:57:12 +0545

    correction for backward

--- practice qs/17th day/loops_with_array.js
+++ practice qs/17th day/loops_with_array.js
@@ -1,9 +1,10 @@
-let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+ let fruits = ["mango", "apple", "banana", "litchi", "orange"];
 for (let i = 0; i <fruits.length; i++){
     console.log(fruits[i]);
 }
 console.log("backward");
 
-for (let i = 0; i >= fruits.length - 1; i--){
+
+for (let i = fruits.length - 1; i >= 0; i--){
     console.log(fruits[i]);
 }
