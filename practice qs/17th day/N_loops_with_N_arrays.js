89eecb2c6faedf13c2bda097acfa0b37082b1c2c
Shiv Raj Pant <shivrajpant76@gamil.com>
2024-10-17 16:44:47 +0545

    nested loops with nested arrays

---
+++ practice qs/17th day/N_loops_with_N_arrays.js
@@ -0,0 +1,18 @@
+let heroes = [["ironman", "spiderman", "thor"], ["superman", "wonder women", "flash"]];
+for (let i = 0 ; i < heroes.length ; i++){
+    console.log(`List # ${i}`);
+    for (let j = 0 ; j < heroes[i].length ; j++){
+        console.log(heroes[i][j]);
+    }
+}
+console.log("NEXT EXAMPLE.........");
+
+
+let student = [["shiv", "95"], ["jaya", "90"], ["sushil", "85"]];
+for (let i = 0 ; i < student.length; i++){
+    console.log(`info of student #${i+1}`);
+    for (let j = 0 ; j < student[i].length; j++){
+        console.log(student[i][j]);
+    }
+}
+        
