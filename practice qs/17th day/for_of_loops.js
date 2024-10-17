6441a07345d11c5e7559af12c05b847d69f4a61e
Shiv Raj Pant <shivrajpant76@gamil.com>
2024-10-17 18:03:37 +0545

    add for of loops

---
+++ practice qs/17th day/for_of_loops.js
@@ -0,0 +1,13 @@
+console.log ("1: FOR ARRAYS...");
+
+let fruits = ["mango", "apple", "banana", "litchi", "orange"];
+for (fruit of fruits) {
+    console.log(fruit)
+}
+
+console.log("2: FOR STRINg....");
+
+for(char of "dreambig"){
+    console.log(char);
+}
+
